1aea955d9985c406661ef9572f9fb9b2945688c0
m4rkbello <markbello10041999@gmail.com>
2025-12-12 22:46:22 +0800

    INSTALLING TANSTACK  QUERY

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,9 @@
 import { Elysia, t } from 'elysia'
 
-const app = new Elysia({ prefix: '/api' }).get('/user', ({name: "MARK BELLO"}))
+const App = new Elysia({ prefix: '/api' }).get('/user', ({name: "MARK BELLO"}))
 
-export const GET = app.fetch 
-export const POST = app.fetch 
+
+export type App = typeof App 
+
+export const GET = App.fetch 
+export const POST = App.fetch 
--- src/lib/eden.ts
+++
@@ -1,5 +0,0 @@
-import { treaty } from '@elysiajs/eden'
-import type { app } from '../app/api/[[...slugs]]/route'
-
-// this require .api to enter /api prefix
-export const api = treaty<app>('localhost:3000').api
