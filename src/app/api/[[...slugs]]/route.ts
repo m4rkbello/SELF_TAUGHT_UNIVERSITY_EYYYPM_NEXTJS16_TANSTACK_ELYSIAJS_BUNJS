e5a532f683228c522db3ff534e64e41828315eac
m4rkbello <markbello10041999@gmail.com>
2025-12-12 21:58:51 +0800

    ADD FOLDER FOR ELYSIAJS/EDEN FOR NEXTJS INTEGRATION OHAHAYS

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,7 +1,6 @@
 import { Elysia, t } from 'elysia'
 
-const app = new Elysia({ prefix: '/api' })
-    .get('/elysia', 'Hello Nextjs')
+const app = new Elysia({ prefix: '/api' }).get('/user', ({name: "MARK BELLO"}))
 
 export const GET = app.fetch 
 export const POST = app.fetch 
