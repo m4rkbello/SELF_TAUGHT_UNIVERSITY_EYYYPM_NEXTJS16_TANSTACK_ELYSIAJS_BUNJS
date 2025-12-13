765af8d96a8bbebda2af78aad2611d25ada0d4b5
m4rkbello <markbello10041999@gmail.com>
2025-12-13 21:32:42 +0800

    INSTALL UPSTASH-REDIS FOR DATABASE

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,9 +1,15 @@
 import { Elysia, t } from 'elysia'
 
-const App = new Elysia({ prefix: '/api' }).get('/user', ({name: "MARK BELLO"}))
+const rooms = new Elysia({prefix: "/room"}).post("/create",()=>{
+    console.log("CREATE A NEW ROOM!")
+})
+
+const App = new Elysia({ prefix: '/api' }).use(rooms)
 
 
-export type App = typeof App 
 
 export const GET = App.fetch 
 export const POST = App.fetch 
+
+
+export type App = typeof App 
