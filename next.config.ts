14e49b9bc2ab406bebf2a6ec25dec305661438cc
m4rkbello <markbello10041999@gmail.com>
2025-12-13 21:50:25 +0800

    ADD AND ALLOW ORIGIN FOR CROSS ORIGIN RESOURCES SHARING - CORS WITH NEXT.CONFIG.TS

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.0.34:3000",
+  ],
 };
 
 export default nextConfig;
