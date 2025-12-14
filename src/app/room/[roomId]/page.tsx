1386daea039c452c9aca6af89e900562bdc1fff7
m4rkbello <markbello10041999@gmail.com>
2025-12-14 11:02:04 +0800

    Adding the frontend design referencing to the pomodoro

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -3,6 +3,14 @@
 import { useParams } from "next/navigation"
 import { useState } from "react"
 
+function formatTimeRemaining(seconds: number){
+    const mins = Math.floor(seconds / 60)
+    const secs = seconds % 60
+
+    return `${mins}:${secs.toString().padStart(2,"0")}`
+}
+
+
 const Page = () => {
 
     const params = useParams()
@@ -43,12 +51,38 @@ const Page = () => {
                         {timeRemaining !== null ? formatTimeRemaining(timeRemaining) : "--:--"}
                     </span>
                 </div>
-
-
             </div>  
 
-
+            <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all group flex items-center gap-2 disabled:opacity-50">
+                <span className="group-hover:animate-pulse">
+                💣
+                </span>
+                DESTROY NOW
+            </button>
         </header>
+
+
+        <div className="flex-1 overflow-auto p-4 space-y-4 scrollbar-thin">
+
+        </div>
+
+        <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+            <div className="flex gap-4">
+                <div className="flex-1 relative group">
+
+                    <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+                        {">"}
+                        <input type="text" className="w-full bg-black border border-zinc-800 focus-border-700 focus-outline-none transition-colors text-zinc-100 placeholder:text-zinc-700" />
+                    </span>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
     </main>
 }
 
