6f6c754514b2af45dbcd6abdfda465f50f2e5ac9
m4rkbello <markbello10041999@gmail.com>
2025-12-14 21:40:25 +0800

    added

---
+++ src/app/api/[[...slugs]]/auth.ts
@@ -0,0 +1,15 @@
+import Elysia from "elysia"
+
+class AuthError extends Error {
+    constructor(message: string){
+        super(message)
+        this.name = "AuthError"
+    }
+}
+
+export const  authMiddleware  =  new Elysia({
+    name: "auth"
+})
+.error({AuthError})
+.onError(({code, set}) => {})
+.derive(() =>{})
