0ad13cffc2acd3df7496b71628c216ae7d8b9e49
m4rkbello <markbello10041999@gmail.com>
2025-12-14 14:19:52 +0800

    Adding the input fields and  UI  of it

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useParams } from "next/navigation"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function formatTimeRemaining(seconds: number){
     const mins = Math.floor(seconds / 60)
@@ -15,6 +15,9 @@ const Page = () => {
 
     const params = useParams()
     const roomId = params.roomId as string
+
+    const [input, setInput] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null)
 
     const [copyStatus, setCopyStatus] = useState("COPY");
     const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
@@ -61,7 +64,6 @@ const Page = () => {
             </button>
         </header>
 
-
         <div className="flex-1 overflow-auto p-4 space-y-4 scrollbar-thin">
 
         </div>
@@ -69,17 +71,26 @@ const Page = () => {
         <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
             <div className="flex gap-4">
                 <div className="flex-1 relative group">
-
                     <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
                         {">"}
-                        <input type="text" className="w-full bg-black border border-zinc-800 focus-border-700 focus-outline-none transition-colors text-zinc-100 placeholder:text-zinc-700" />
+                       <input
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyDown={(e) => {
+                            if (e.key === "Enter" && input.trim()) {
+                                // TODO:   SEND MESSAGE
+                                inputRef.current?.focus()
+                            }
+                            }}
+                            type="text"
+                            placeholder="Type a message..."
+                            className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+                            autoFocus
+                        />
                     </span>
-
                 </div>
-
-
+                <button className="bg-zinc-800 text-zinc-400 px-6 tex-sm font-bold hover:text-zinc-200 transition-all disable:opacity-50 disable:cursor-not-allowed cursor-pointer">SEND</button>
             </div>
-
         </div>
 
 
