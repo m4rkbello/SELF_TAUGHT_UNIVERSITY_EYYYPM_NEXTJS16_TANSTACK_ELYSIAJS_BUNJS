6443b6587921e1f6b68abd0ee9986dae42fda3fe
m4rkbello <markbello10041999@gmail.com>
2025-12-18 21:24:45 +0800

    added

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -7,6 +7,7 @@ const ROOM_TTL_SECONDS = 60 * 10
 const rooms = new Elysia({prefix: "/room"}).post("/create", async () => {
 
     const roomId = nanoid()
+    
 
     await redis.hset(`meta:${roomId}`,{
         connected: [],
